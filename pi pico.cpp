7754f5d573e32b0f6add18f0b32b2cf9fd517d46
platesuperiority985 <joyconer0@gmail.com>
2025-05-21 18:19:49 -0700

    Add files via upload

--- pi pico.cpp
+++ pi pico.cpp
@@ -1,26 +1,24 @@
 /*
-
 if you see "// insert comment here" this is a area you can change things for your liking/controller setup
-
 */
 
 #include <PicoGamepad.h>
 PicoGamepad gamepad;
 
 const int buttoncount = 9; // amount of pins in use
-const unsigned int buttonpins[] = {22, 13, 12, 11, 10, 15, 17, 2, 1}; // pins being used (first 5 MUST be GRYBO)
+const unsigned int buttonpins[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // pins being used (first 5 MUST be GRYBO)
 
-unsigned int buttonstate[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // if you added or removed the amount of pins add/remove the correct amount of 0s
-unsigned int prevbuttonstate[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // if you added or removed the amount of pins add/remove the correct amount of 0s
+unsigned int buttonstate[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+unsigned int prevbuttonstate[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-const int hz = 1000; // setting for dequeue poll rate (hz)
-const int debounce = 24; // setting for debounce (ms)
+const int hz = 4000; // setting for dequeue poll rate (hz)
+const int debounce = 0; // setting for debounce (ms)
 
 unsigned long pollrate = 1.0 / hz * 1000000.0;
 
 int queueinput[200];
 int queueactutate[200];
-int front = -1; 
+int front = -1;
 int rear = -1;
 int queuecount = 0;
 
@@ -30,11 +28,14 @@ void setup() {
   for (int i = 0; i < buttoncount; i++)
     pinMode(buttonpins[i], INPUT_PULLUP);
 
+  randomSeed(analogRead(27)); // initialize RNG for overtap randomness
+
   time2 = micros();
 }
 
 void addqueue(int item1, int item2) {
   if (rear == 200 - 1) {
+    // queue full, do nothing
   }
   else {
     if (front == -1)
@@ -48,6 +49,7 @@ void addqueue(int item1, int item2) {
 
 void dequeue() {
   if (front == -1) {
+    // queue empty, do nothing
   }
   else {
     front++;
@@ -72,13 +74,30 @@ void loop() {
       }
     }
   }
-  
+
   unsigned long newTime = micros();
   if (newTime - time2 >= pollrate) {
     if (queuecount) {
-      gamepad.SetButton(queueinput[front], queueactutate[front]);
+      int pin = queueinput[front];
+      int state = queueactutate[front];
+
+      gamepad.SetButton(pin, state);
       gamepad.send_update();
       dequeue();
+
+      // --------- OVERTAP SECTION START ---------
+      if (queuecount > 65 && state == 1) { // only overtap on press
+        int chance = random(0, 100); // 0-99
+        if (chance < 60) { // 60% chance to overtap
+          int overtaps = random(2, 8); // 2-7 extra presses
+          for (int i = 0; i < overtaps; i++) {
+            delayMicroseconds(400); // small delay between overtaps
+            addqueue(pin, 0); // release
+            addqueue(pin, 1); // press again
+          }
+        }
+      }
+      // --------- OVERTAP SECTION END ---------
     }
 
     for (int i = 0; i < 4; i++) {
@@ -95,17 +114,13 @@ void loop() {
         prevbuttonstate[i + 5] = buttonstate[i + 5];
       }
     }
-    
-    int w = analogRead(28);
-    gamepad.SetRz(w);
-    gamepad.send_update();
-    
+
     time2 = newTime;
   } else delayMicroseconds(debounce);
 
   if (queuecount >= 199) {
-    rear = - 1;
-    front = - 1;
+    rear = -1;
+    front = -1;
     queuecount = 0;
   }
 }
